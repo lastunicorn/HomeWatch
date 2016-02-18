ca85064e2438e59b046fcf1b096ada975e82fefd
lastunicorn <coldblackstar@yahoo.com>
2016-02-18 21:45:22 +0200

    removed the Sensor classes.

--- sources/HomeWatch/Alarm.h
+++ sources/HomeWatch/Alarm.h
@@ -1,20 +1,20 @@
 /*
- * Home Watch
- * Copyright (C) 2016  Dust in the Wind
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+   Home Watch
+   Copyright (C) 2016  Dust in the Wind
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef Alarm_h
 #define Alarm_h
@@ -24,8 +24,8 @@
 #include "SmsSender.h"
 
 /*
- * This class is implementing the logic to be executed when the alarm is triggered.
- */
+   This class is implementing the logic to be executed when the alarm is triggered.
+*/
 class Alarm
 {
   public:
--- sources/HomeWatch/SmsSender.cpp
+++ sources/HomeWatch/SmsSender.cpp
@@ -1,20 +1,20 @@
 /*
- * Home Watch
- * Copyright (C) 2016  Dust in the Wind
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+   Home Watch
+   Copyright (C) 2016  Dust in the Wind
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "Arduino.h"
 #include "SmsSender.h"
@@ -115,7 +115,7 @@ void SmsSender::sendSMS(char remoteNumber[20], char txtMsg[200])
     String s = "GSM - Fake send sms to number: ";
     s += remoteNumber;
     logger->write(s);
-    
+
     return;
   }
 
